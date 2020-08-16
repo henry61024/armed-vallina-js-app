b5449204af89884ccc091c80b70652cb65ae9d0f
Henry Chang <henry35208@hotmail.com.tw>
2020-08-16 22:33:10 +0800

    feature(armed-vallina-app): add armed vallina app template

---
+++ src/components/app/index.js
@@ -0,0 +1,19 @@
+import './index.css';
+
+export default class App {
+  constructor(elem) {
+    if (!elem) return;
+    this.elem = elem;
+  }
+
+  render() {
+    if (this.elem) {
+      this.elem.innerHTML = `
+        <section data-component="app">
+        <h1>App Component</h1>
+        <p>The time is: ${(new Date()).toLocaleString()}</p>
+        </section>
+        `;
+    }
+  }
+}
